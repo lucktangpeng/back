22740ab75b092e5cf855edab0d381f34fc6f4f83
唐鹏 <961624856@qq.com>
2019-05-13 17:09:22 +0800

    创建页面开发完毕

---
+++ src/components/Vmodal.vue
@@ -0,0 +1,26 @@
+<<template>
+     <div>
+        <div class="modal fade" tabindex="-1" role="dialog" id="my_modal">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <!-- <h4 class="modal-title">Modal title</h4> -->
+                </div>
+                <div class="modal-body text-center" >
+                    <p>操作成功</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">确定</button>
+                    <!-- <button type="button" class="btn btn-primary">Save changes</button> -->
+                </div>
+                </div><!-- /.modal-content -->
+            </div><!-- /.modal-dialog -->
+        </div><!-- /.modal -->
+    </div>
+</template>
+<script>
+export default {
+    name:"Vmodal"
+}
+</script>
